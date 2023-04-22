8ef16acaeefb15615575043b2c595ab2718fc1b8
lexidr <119054548+lexidr@users.noreply.github.com>
2023-04-22 15:58:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,63 @@
 import logging
 from telegram.ext import Application, MessageHandler, filters, CommandHandler, ConversationHandler
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 import datetime
 
 BOT_TOKEN = '6032759993:AAFbXq-hofzHYQQtRLAH9XosRZFRajlDKQc'
 
-# Запускаем логгирование
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
 )
 
 logger = logging.getLogger(__name__)
 
+reply_keyboard = [['/show_events', '/films'],
+                  ['/edit']]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+
 
 async def start(update, context):
     await update.message.reply_text(
-        "Привет. Пройдите небольшой опрос, пожалуйста!\n"
-        "Вы можете прервать опрос, послав команду /stop.\n"
-        "В каком городе вы живёте?")
-
-    # Число-ключ в словаре states —
-    # втором параметре ConversationHandler'а.
+        "Привет. Это телеграмм бот WhereToGo!\n"
+        "Я могу рассказать тебе всё о мероприятиях в твоём городе.\n"
+        "Но сначала ответь на пару вопросов.\n"
+        "Сколько тебе лет?")
     return 1
-    # Оно указывает, что дальше на сообщения от этого пользователя
-    # должен отвечать обработчик states[1].
-    # До этого момента обработчиков текстовых сообщений
-    # для этого пользователя не существовало,
-    # поэтому текстовые сообщения игнорировались.
 
 
-# Добавили словарь user_data в параметры.
-async def first_response(update, context):
-    # Сохраняем ответ в словаре.
+async def enter_age(update, context):
+    a = update.message.text
+    if a.isdigit() and int(a) > 0:
+        context.user_data['age'] = a
+        await update.message.reply_text("В каком городе ты живёшь?")
+        return 2
+    else:
+        await update.message.reply_text("Неверный формат ввода. Попробуйте еще раз.")
+        return 1
+
+
+async def enter_city(update, context):
     context.user_data['locality'] = update.message.text
-    await update.message.reply_text(
-        f"Какая погода в городе {context.user_data['locality']}?")
-    return 2
+    await update.message.reply_text("Спасибо! Теперь я смогу подобрать для тебя подходящие мероприятия!)")
+    return 3
 
 
-# Добавили словарь user_data в параметры.
-async def second_response(update, context):
-    weather = update.message.text
-    logger.info(weather)
-    # Используем user_data в ответе.
-    await update.message.reply_text(
-        f"Спасибо за участие в опросе! Привет, {context.user_data['locality']}!")
-    context.user_data.clear()  # очищаем словарь с пользовательскими данными
+async def show_events(update, context):
+    await update.message.reply_text(f"Вот такие вот мероприятия для {context.user_data['age']} {context.user_data['locality']}")
     return ConversationHandler.END
 
+
+async def edit(update, context):
+    await update.message.reply_text(
+        "Ты хочешь изменить информацию о себе.\n"
+        "Вы можете прервать опрос, послав команду /stop.\n"
+        "Сколько тебе лет?")
+    return 1
+
+
 async def stop(update, context):
-    await update.message.reply_text("Всего доброго!")
+    await update.message.reply_text("Надеюсь, я смог вам помочь!")
     return ConversationHandler.END
 
 
@@ -56,24 +65,33 @@ def main():
     application = Application.builder().token(BOT_TOKEN).build()
 
     conv_handler = ConversationHandler(
-        # Точка входа в диалог.
-        # В данном случае — команда /start. Она задаёт первый вопрос.
         entry_points=[CommandHandler('start', start)],
 
-        # Состояние внутри диалога.
-        # Вариант с двумя обработчиками, фильтрующими текстовые сообщения.
         states={
-            # Функция читает ответ на первый вопрос и задаёт второй.
-            1: [MessageHandler(filters.TEXT & ~filters.COMMAND, first_response)],
-            # Функция читает ответ на второй вопрос и завершает диалог.
-            2: [MessageHandler(filters.TEXT & ~filters.COMMAND, second_response)]
+            1: [MessageHandler(filters.TEXT & ~filters.COMMAND, enter_age)],
+            2: [MessageHandler(filters.TEXT & ~filters.COMMAND, enter_city)],
+            3: [MessageHandler(filters.TEXT & ~filters.COMMAND, show_events)]
         },
 
-        # Точка прерывания диалога. В данном случае — команда /stop.
+        fallbacks=[CommandHandler('stop', stop)]
+    )
+
+    conv_edit = ConversationHandler(
+        entry_points=[CommandHandler('edit', edit)],
+
+        states={
+            1: [MessageHandler(filters.TEXT & ~filters.COMMAND, enter_age)],
+            2: [MessageHandler(filters.TEXT & ~filters.COMMAND, enter_city)]
+        },
+
         fallbacks=[CommandHandler('stop', stop)]
     )
 
     application.add_handler(conv_handler)
+    application.add_handler(conv_edit)
+
+    application.add_handler(CommandHandler("show_events", show_events))
+    application.add_handler(CommandHandler("help", help))
 
     application.run_polling()
 
